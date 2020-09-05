4ac17e107793dadebcbb131803113355a577f567
RJ Wright <rj@rjwright.me>
2020-09-06 00:20:31 +1000

    Sort leaderboard highest to lowest score. Update README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is a quick quiz to test your understanding of beginner JavaScript concepts.
 1. Responsive, mobile first design
 1. Visual timer and progress bar
 1. Leaderboard that uses local storage to remember previous scores
+1. Leaderboard sorts itself to display the highest scores first
 
 ## Link
 
@@ -13,4 +14,9 @@ https://rjwrightme.github.io/javascript-quiz/
 ## Screenshot
 
 The following image demonstrates the application functionality:
-[comment]: <> (![password generator demo](./assets/PG-Screen-Shot.png))
+![JavaScript Quiz Homepage](./assets/images/screenshots/homepage.png)
+![JavaScript Quiz](./assets/images/screenshots/quiz.png)
+![JavaScript Quiz Correct Answer](./assets/images/screenshots/correct.png)
+![JavaScript Quiz Wrong Answer](./assets/images/screenshots/incorrect.png)
+![JavaScript Quiz Completed](./assets/images/screenshots/finished.png)
+![JavaScript Quiz Leaderboard](./assets/images/screenshots/leaderboard.png)
--- assets/score.js
+++ assets/score.js
@@ -15,6 +15,7 @@ function renderHighScores() {
     readLocalStorage();
     // if we do find saved scores, loop through them and add them to the DOM
     if (savedScores != undefined) {
+        sortedScores = savedScores.sort(sortHighScores);
         for (let i = 0; i < savedScores.length; i++) {
             let score = document.createElement('li');
             score.textContent = `${savedScores[i].name} -> ${savedScores[i].score}`;
@@ -25,6 +26,20 @@ function renderHighScores() {
         noScore.textContent = 'There are currently no saved scores. Be sure to save your score after completing the quiz to bask in your own glory.';
         scoreList.appendChild(noScore);
     }
+}
+
+// Sort High Scores in order from highest score to lowest
+function sortHighScores(a, b) {
+    const aScore = a.score;
+    const bScore = b.score;
+
+    let comparison = 0;
+    if (aScore < bScore) {
+        comparison = 1;
+    } else {
+        comparison = -1;
+    }
+    return comparison;
 }
 
 renderHighScores();
